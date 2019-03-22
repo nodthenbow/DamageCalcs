1b92921ede916d9c34d7d9dee34ecf03895236de
nodthenbow <gsteier@sasktel.net>
2019-03-21 23:30:16 -0600

    Update reversaledgefinder.js

--- reversaledgefinder.js
+++ reversaledgefinder.js
@@ -1,8 +1,8 @@
 
 <!-- Begin
 
-var A=[],str=[],x=[],y=[],mat=[],dvals=[];
-var m,m1,n,n1,n2,mess,val,vsave,r2ev,r2gb1ev,r2gb2ev,foundev;
+var A=[],str=[],x=[],y=[],mat=[],dvals=[],mathold=[];
+var m,m1,n,n1,n2,mess,val,vsave,r2ev,r2gb1ev,r2gb2ev,foundev,matrix;
 
 /*mat is the matrix
 
@@ -12,15 +12,475 @@ var m,m1,n,n1,n2,mess,val,vsave,r2ev,r2gb1ev,r2gb2ev,foundev;
 
 function main(form) {
 	calcrounds(form);
-	
+	/*
 	display(form);
 	form.output.value=mess; 
+	*/
 }
 	
+function calcrounds(form){
+	for(i = 0; i<49;i++){
+		dvals[i] = 0;
+	}
+	
+	dvals[0]=form.p1r2a.value;
+	dvals[1]=form.p1r2b.value;
+	dvals[2]=form.p1r2k.value;
+	dvals[3]=form.p1r2g.value;
+	dvals[5]=form.p16.value;
+	dvals[6]=form.p14.value;
+	dvals[7]=form.p12.value;
+	dvals[8]=-1*form.p2r2a.value;
+	dvals[9]=-1*form.p2r2b.value;
+	dvals[10]=-1*form.p2r2k.value;
+	dvals[11]=-1*form.p2r2g.value;
+	dvals[12]=-1*form.p26.value;
+	dvals[13]=-1*form.p24.value;
+	dvals[14]=-1*form.p22.value;
+	dvals[15]=form.p1gba.value;
+	dvals[16]=form.p1gbb.value;
+	dvals[17]=form.p1gbk.value;
+	dvals[18]=-1*form.p2gba.value;
+	dvals[19]=-1*form.p2gbb.value;
+	dvals[20]=-1*form.p2gbk.value;
+	dvals[21]=form.giworthp1.value;
+	dvals[22]=form.giworthp2.value;
+	
+	dvals[15]=0;
+	dvals[16]=0;
+	dvals[17]=0;
+	dvals[18]=0;
+	dvals[19]=0;
+	dvals[20]=0;
+	
+	if(form.gbriskp1.checked){
+		dvals[18]=-1*form.p2gba.value;
+		dvals[19]=-1*form.p2gbb.value;
+		dvals[20]=-1*form.p2gbk.value;
+	}
+	if(form.gbriskp2.checked){
+		dvals[15]=form.p1gba.value;
+		dvals[16]=form.p1gbb.value;
+		dvals[17]=form.p1gbk.value;
+	}
+	if(form.usepercents.checked){
+		for(i=0;i<23;i++){
+			dvals[i] = dvals[i]/280.0;
+		}
+	}
+	
+	mathold[0]= dvals[0];
+	mathold[1]= dvals[9];
+	mathold[2]= dvals[0];
+	mathold[3]= dvals[15];
+	mathold[4]= dvals[12];
+	mathold[5]= dvals[0];
+	mathold[6]= dvals[0];
+	mathold[7]= dvals[1];
+	mathold[8]= dvals[9];
+	mathold[9]= dvals[1];
+	mathold[10]= dvals[16];
+	mathold[11]= dvals[1];
+	mathold[12]= dvals[1];
+	mathold[13]= dvals[14];
+	mathold[14]= dvals[8];
+	mathold[15]= dvals[2];
+	mathold[16]= dvals[2];
+	mathold[17]= dvals[17];
+	mathold[18]= dvals[2];
+	mathold[19]= dvals[13];
+	mathold[20]= dvals[2];
+	mathold[21]= dvals[18];
+	mathold[22]= dvals[19];
+	mathold[23]= dvals[20];
+	mathold[24]= dvals[21];
+	mathold[25]= dvals[25];
+	mathold[26]= dvals[26];
+	mathold[27]= dvals[27];
+	mathold[28]= dvals[5];
+	mathold[29]= dvals[9];
+	mathold[30]= dvals[10];
+	mathold[31]= dvals[31];
+	mathold[32]= dvals[32];
+	mathold[33]= dvals[33];
+	mathold[34]= dvals[34];
+	mathold[35]= dvals[8];
+	mathold[36]= dvals[9];
+	mathold[37]= dvals[6];
+	mathold[38]= dvals[38];
+	mathold[39]= dvals[39];
+	mathold[40]= dvals[40];
+	mathold[41]= dvals[41];
+	mathold[42]= dvals[8];
+	mathold[43]= dvals[7];
+	mathold[44]= dvals[10];
+	mathold[45]= dvals[45];
+	mathold[46]= dvals[46];
+	mathold[47]= dvals[47];
+	mathold[48]= dvals[48];
+	
+	document.getElementById("outputbox").innerHTML += "Round 2 after a clash</br>";
+	matrix = "";
+	for(hq=0; hq<49;hq++){
+		matrix += mathold[hq] +  " ";
+		if((hq+1)%7==0){
+			matrix += "\n";
+		}
+	}
+	getinput(form);
+	InitA();
+	makeValuePositive();
+	Solve();
+	ValueOptimalStrategies();
+	display(form);
+	
+	r2ev = foundev;
+	
+	dvals[0]=form.p1r2a.value;
+	dvals[1]=form.p1r2b.value;
+	dvals[2]=form.p1r2k.value;
+	dvals[3]=form.p1r2g.value;
+	dvals[5]=form.p16.value;
+	dvals[6]=form.p14.value;
+	dvals[7]=form.p12.value;
+	dvals[8]=-1*form.p2r2a.value;
+	dvals[9]=-1*form.p2r2b.value;
+	dvals[10]=-1*form.p2r2k.value;
+	dvals[11]=-1*form.p2r2g.value;
+	dvals[12]=-1*form.p26.value;
+	dvals[13]=-1*form.p24.value;
+	dvals[14]=-1*form.p22.value;
+	dvals[15]=form.p1gba.value;
+	dvals[16]=form.p1gbb.value;
+	dvals[17]=form.p1gbk.value;
+	dvals[18]=-1*form.p2gba.value;
+	dvals[19]=-1*form.p2gbb.value;
+	dvals[20]=-1*form.p2gbk.value;
+	dvals[21]=form.giworthp1.value;
+	dvals[22]=form.giworthp2.value;
+	
+	dvals[15]=0;
+	dvals[16]=0;
+	dvals[17]=0;
+	
+	if(form.gbriskp1.checked){
+		dvals[18]=-1*form.p2gba.value;
+		dvals[19]=-1*form.p2gbb.value;
+		dvals[20]=-1*form.p2gbk.value;
+	}
+	if(form.gbriskp2.checked){
+		dvals[15]=form.p1gba.value;
+		dvals[16]=form.p1gbb.value;
+		dvals[17]=form.p1gbk.value;
+	}
+	if(form.usepercents.checked){
+		for(i=0;i<23;i++){
+			dvals[i] = dvals[i]/280.0;
+		}
+	}
+	
+	mathold[0]= dvals[0];
+	mathold[1]= dvals[9];
+	mathold[2]= dvals[0];
+	mathold[3]= dvals[15];
+	mathold[4]= dvals[12];
+	mathold[5]= dvals[0];
+	mathold[6]= dvals[0];
+	mathold[7]= dvals[1];
+	mathold[8]= dvals[9];
+	mathold[9]= dvals[1];
+	mathold[10]= dvals[16];
+	mathold[11]= dvals[1];
+	mathold[12]= dvals[1];
+	mathold[13]= dvals[14];
+	mathold[14]= dvals[8];
+	mathold[15]= dvals[2];
+	mathold[16]= dvals[2];
+	mathold[17]= dvals[17];
+	mathold[18]= dvals[2];
+	mathold[19]= dvals[13];
+	mathold[20]= dvals[2];
+	mathold[21]= dvals[18];
+	mathold[22]= dvals[19];
+	mathold[23]= dvals[20];
+	mathold[24]= dvals[21];
+	mathold[25]= dvals[25];
+	mathold[26]= dvals[26];
+	mathold[27]= dvals[27];
+	mathold[28]= dvals[5];
+	mathold[29]= dvals[9];
+	mathold[30]= dvals[10];
+	mathold[31]= dvals[31];
+	mathold[32]= dvals[32];
+	mathold[33]= dvals[33];
+	mathold[34]= dvals[34];
+	mathold[35]= dvals[8];
+	mathold[36]= dvals[9];
+	mathold[37]= dvals[6];
+	mathold[38]= dvals[38];
+	mathold[39]= dvals[39];
+	mathold[40]= dvals[40];
+	mathold[41]= dvals[41];
+	mathold[42]= dvals[8];
+	mathold[43]= dvals[7];
+	mathold[44]= dvals[10];
+	mathold[45]= dvals[45];
+	mathold[46]= dvals[46];
+	mathold[47]= dvals[47];
+	mathold[48]= dvals[48];
+	
+	document.getElementById("outputbox").innerHTML += "Round 2 after P1's B is blocked</br>";
+	matrix = "";
+	for(hq=0; hq<49;hq++){
+		matrix += mathold[hq] +  " ";
+		if((hq+1)%7==0){
+			matrix += "\n";
+		}
+	}
+	getinput(form);
+	InitA();
+	makeValuePositive();
+	Solve();
+	ValueOptimalStrategies();
+	display(form);
+	
+	r2gb2ev = foundev;
+	
+	dvals[0]=form.p1r2a.value;
+	dvals[1]=form.p1r2b.value;
+	dvals[2]=form.p1r2k.value;
+	dvals[3]=form.p1r2g.value;
+	dvals[5]=form.p16.value;
+	dvals[6]=form.p14.value;
+	dvals[7]=form.p12.value;
+	dvals[8]=-1*form.p2r2a.value;
+	dvals[9]=-1*form.p2r2b.value;
+	dvals[10]=-1*form.p2r2k.value;
+	dvals[11]=-1*form.p2r2g.value;
+	dvals[12]=-1*form.p26.value;
+	dvals[13]=-1*form.p24.value;
+	dvals[14]=-1*form.p22.value;
+	dvals[15]=form.p1gba.value;
+	dvals[16]=form.p1gbb.value;
+	dvals[17]=form.p1gbk.value;
+	dvals[18]=-1*form.p2gba.value;
+	dvals[19]=-1*form.p2gbb.value;
+	dvals[20]=-1*form.p2gbk.value;
+	dvals[21]=form.giworthp1.value;
+	dvals[22]=form.giworthp2.value;
+	
+	dvals[18]=0;
+	dvals[19]=0;
+	dvals[20]=0;
+	
+	if(form.gbriskp1.checked){
+		dvals[18]=-1*form.p2gba.value;
+		dvals[19]=-1*form.p2gbb.value;
+		dvals[20]=-1*form.p2gbk.value;
+	}
+	if(form.gbriskp2.checked){
+		dvals[15]=form.p1gba.value;
+		dvals[16]=form.p1gbb.value;
+		dvals[17]=form.p1gbk.value;
+	}
+	if(form.usepercents.checked){
+		for(i=0;i<23;i++){
+			dvals[i] = dvals[i]/280.0;
+		}
+	}
+	
+	mathold[0]= dvals[0];
+	mathold[1]= dvals[9];
+	mathold[2]= dvals[0];
+	mathold[3]= dvals[15];
+	mathold[4]= dvals[12];
+	mathold[5]= dvals[0];
+	mathold[6]= dvals[0];
+	mathold[7]= dvals[1];
+	mathold[8]= dvals[9];
+	mathold[9]= dvals[1];
+	mathold[10]= dvals[16];
+	mathold[11]= dvals[1];
+	mathold[12]= dvals[1];
+	mathold[13]= dvals[14];
+	mathold[14]= dvals[8];
+	mathold[15]= dvals[2];
+	mathold[16]= dvals[2];
+	mathold[17]= dvals[17];
+	mathold[18]= dvals[2];
+	mathold[19]= dvals[13];
+	mathold[20]= dvals[2];
+	mathold[21]= dvals[18];
+	mathold[22]= dvals[19];
+	mathold[23]= dvals[20];
+	mathold[24]= dvals[21];
+	mathold[25]= dvals[25];
+	mathold[26]= dvals[26];
+	mathold[27]= dvals[27];
+	mathold[28]= dvals[5];
+	mathold[29]= dvals[9];
+	mathold[30]= dvals[10];
+	mathold[31]= dvals[31];
+	mathold[32]= dvals[32];
+	mathold[33]= dvals[33];
+	mathold[34]= dvals[34];
+	mathold[35]= dvals[8];
+	mathold[36]= dvals[9];
+	mathold[37]= dvals[6];
+	mathold[38]= dvals[38];
+	mathold[39]= dvals[39];
+	mathold[40]= dvals[40];
+	mathold[41]= dvals[41];
+	mathold[42]= dvals[8];
+	mathold[43]= dvals[7];
+	mathold[44]= dvals[10];
+	mathold[45]= dvals[45];
+	mathold[46]= dvals[46];
+	mathold[47]= dvals[47];
+	mathold[48]= dvals[48];
+	
+	document.getElementById("outputbox").innerHTML += "Round 2 after P2's B is blocked</br>";
+	matrix = "";
+	for(hq=0; hq<49;hq++){
+		matrix += mathold[hq] +  " ";
+		if((hq+1)%7==0){
+			matrix += "\n";
+		}
+	}
+	
+	getinput(form);
+	InitA();
+	makeValuePositive();
+	Solve();
+	ValueOptimalStrategies();
+	display(form);
+	
+	r2gb1ev = foundev;
+	
+	dvals[0]=form.p1r1a.value;
+	dvals[1]=form.p1r1b.value;
+	dvals[2]=form.p1r1k.value;
+	dvals[3]=form.p1r1g.value;
+	dvals[8]=-1*form.p2r1a.value;
+	dvals[9]=-1*form.p2r1b.value;
+	dvals[10]=-1*form.p2r1k.value;
+	dvals[11]=-1*form.p2r1g.value;
+	dvals[12]=-1*form.p26.value;
+	dvals[13]=-1*form.p24.value;
+	dvals[14]=-1*form.p22.value;
+	dvals[15]=form.p1gba.value;
+	dvals[16]=form.p1gbb.value;
+	dvals[17]=form.p1gbk.value;
+	dvals[18]=-1*form.p2gba.value;
+	dvals[19]=-1*form.p2gbb.value;
+	dvals[20]=-1*form.p2gbk.value;
+	dvals[21]=form.giworthp1.value;
+	dvals[22]=form.giworthp2.value;
+	
+	/*
+	if(form.gbriskp1.checked){
+		dvals[18]=-1*form.p2gba.value;
+		dvals[19]=-1*form.p2gbb.value;
+		dvals[20]=-1*form.p2gbk.value;
+	}
+	if(form.gbriskp2.checked){
+		dvals[15]=form.p1gba.value;
+		dvals[16]=form.p1gbb.value;
+		dvals[17]=form.p1gbk.value;
+	}
+	*/
+	if(form.usepercents.checked){
+		for(i=0;i<23;i++){
+			dvals[i] = dvals[i]/280.0;
+		}
+	}
+	
+	mathold[0]= r2ev;
+	mathold[1]= dvals[9];
+	mathold[2]= dvals[0];
+	mathold[3]= dvals[15];
+	mathold[4]= dvals[12];
+	mathold[5]= dvals[0];
+	mathold[6]= dvals[0];
+	mathold[7]= dvals[1];
+	mathold[8]= r2ev;
+	mathold[9]= dvals[1];
+	mathold[10]= r2gb1ev;
+	mathold[11]= dvals[1];
+	mathold[12]= dvals[1];
+	mathold[13]= dvals[14];
+	mathold[14]= dvals[8];
+	mathold[15]= dvals[2];
+	mathold[16]= r2ev;
+	mathold[17]= dvals[17];
+	mathold[18]= dvals[2];
+	mathold[19]= dvals[13];
+	mathold[20]= dvals[2];
+	mathold[21]= dvals[18];
+	mathold[22]= r2gb2ev;
+	mathold[23]= dvals[20];
+	mathold[24]= r2ev;
+	mathold[25]= dvals[25];
+	mathold[26]= dvals[26];
+	mathold[27]= dvals[27];
+	mathold[28]= dvals[5];
+	mathold[29]= dvals[9];
+	mathold[30]= dvals[10];
+	mathold[31]= dvals[31];
+	mathold[32]= dvals[32];
+	mathold[33]= dvals[33];
+	mathold[34]= dvals[34];
+	mathold[35]= dvals[8];
+	mathold[36]= dvals[9];
+	mathold[37]= dvals[6];
+	mathold[38]= dvals[38];
+	mathold[39]= dvals[39];
+	mathold[40]= dvals[40];
+	mathold[41]= dvals[41];
+	mathold[42]= dvals[8];
+	mathold[43]= dvals[7];
+	mathold[44]= dvals[10];
+	mathold[45]= dvals[45];
+	mathold[46]= dvals[46];
+	mathold[47]= dvals[47];
+	mathold[48]= dvals[48];
+	
+	document.getElementById("outputbox").innerHTML += "Round 1</br>";
+	matrix = "";
+	for(hq=0; hq<49;hq++){
+		matrix += mathold[hq] +  " ";
+		if((hq+1)%7==0){
+			matrix += "\n";
+		}
+	}
+	console.log(matrix);
+	getinput(form);
+	InitA();
+	makeValuePositive();
+	Solve();
+	ValueOptimalStrategies();
+	display(form);
+}
+
+function getinput(form){
+	var z;
+	for (i=0;i<matrix.length;i++) {
+		z=matrix.charCodeAt(i);
+		if ((z==13)||(z==10)) break;
+	}
+	if (z==13) {
+		mat = matrix.split("\r");  /*for Explorer*/
+	} else if (z==10) {
+		mat = matrix.split("\n");  /*for Netscape*/
+	}
+	m=mat.length;
+	while (mat[m-1].length==0) m--;
+	for (i=0;i<m;i++) {
+		mat[i]=mat[i].split(" ");
+	}
+}
 
 function InitA() {
-	A=[];
-	m=7;
 	var i,j,k;
 	m1=m+1;
 	for (i=0;i<=m1;i++) {
@@ -49,7 +509,7 @@ function InitA() {
 		A[m1][j] = -1;     /* Last row */
 	}
 	A[m1][n1]=0;          /* Lower Right corner */
-	mess = "The matrix is\r\n";
+	mess = "The matrix is<br>";
 	i=1;
 	while (i<m1) {
 		str[i]=""
@@ -58,7 +518,7 @@ function InitA() {
 			str[i]+=" "+ A[i][j];
 			j++;
 		}
-		mess += str[i]+"\r\n";
+		mess += str[i]+"</br>";
 		i++;
 	}
 }
@@ -88,7 +548,7 @@ function makeValuePositive() {
 
 function CheckA() {
 	var i,j;
-	mess1 = "The matrix is\n";
+	mess1 = "The matrix is</br>";
 	i=0;
 	while (i<=m1) {
 		str[i]=""
@@ -97,7 +557,7 @@ function CheckA() {
 			str[i]+=" "+ A[i][j];
 			j++;
 		}
-		mess1 += str[i]+"\r";
+		mess1 += str[i]+"</br>";
 		i++;
 	}
 	alert(mess1)
@@ -192,325 +652,30 @@ function display(form) {
 	val=val+vsave;
 	var t=Math.round(100000*val)/100000;
 	foundev = Math.round(100000*val)/100000;
-	mess+= "The value is "+t+".\r\n";
+	mess+= "The value is "+t+".</br>";
 	var t=Math.round(100000*x[1])/100000;
-	opt1= "An optimal strategy for Player 1 is:\r\n ("+t;
+	opt1= "An optimal strategy for Player 1 is:</br> ("+t;
 	i=2;
 	while (i<=m) {
 		t=Math.round(100000*x[i])/100000;
 		opt1+= ","+t;
 		i++;
 	}
-	opt1+=")\r"; 
+	opt1+=")</br>"; 
 	var t=Math.round(100000*y[1])/100000;
-	opt2="An optimal strategy for Player 2 is:\r\n ("+t;
+	opt2="An optimal strategy for Player 2 is:</br> ("+t;
 	j=2;
 	while (j<=n) {
 		t=Math.round(100000*y[j])/100000;
 		opt2+= ","+t;
 		j++;
 	}
-	opt2+=")\r\n"; 
+	opt2+=")</br>"; 
 	mess+=opt1+opt2; 
-	document.getElementById("outputbox").innerHTML += mess+"\r\n"; 
+	document.getElementById("outputbox").innerHTML += mess+"</br>"; 
 }
 
-function calcrounds(form){
-	dvals[0]=form.p1r2a.value;
-	dvals[1]=form.p1r2b.value;
-	dvals[2]=form.p1r2k.value;
-	dvals[3]=form.p1r2g.value;
-	dvals[5]=form.p16.value;
-	dvals[6]=form.p14.value;
-	dvals[7]=form.p12.value;
-	dvals[8]=-1*form.p2r2a.value;
-	dvals[9]=-1*form.p2r2b.value;
-	dvals[10]=-1*form.p2r2k.value;
-	dvals[11]=-1*form.p2r2g.value;
-	dvals[12]=-1*form.p26.value;
-	dvals[13]=-1*form.p24.value;
-	dvals[14]=-1*form.p22.value;
-	dvals[15]=form.p1gba.value;
-	dvals[16]=form.p1gbb.value;
-	dvals[17]=form.p1gbk.value;
-	dvals[18]=-1*form.p2gba.value;
-	dvals[19]=-1*form.p2gbb.value;
-	dvals[20]=-1*form.p2gbk.value;
-	dvals[21]=form.giworthp1.value;
-	dvals[22]=form.giworthp2.value;
-	
-	dvals[15]=0;
-	dvals[16]=0;
-	dvals[17]=0;
-	
-	if(gbriskp1){
-		dvals[18]=-1*form.p2gba.value;
-		dvals[19]=-1*form.p2gbb.value;
-		dvals[20]=-1*form.p2gbk.value;
-	}
-	if(gbriskp2){
-		dvals[15]=form.p1gba.value;
-		dvals[16]=form.p1gbb.value;
-		dvals[17]=form.p1gbk.value;
-	}
-	if(form.bypercents.value){
-		for(i=0;i<23;i++){
-			dvals[i] = dvals[i]/280.0;
-		}
-	}
-	
-	mat[0]= dvals[0];
-	mat[1]= dvals[9];
-	mat[2]= dvals[0];
-	mat[3]= dvals[15];
-	mat[4]= dvals[12];
-	mat[5]= dvals[0];
-	mat[6]= dvals[0];
-	mat[7]= dvals[1];
-	mat[8]= dvals[9];
-	mat[9]= dvals[1];
-	mat[10]= dvals[16];
-	mat[11]= dvals[1];
-	mat[12]= dvals[1];
-	mat[13]= dvals[14];
-	mat[14]= dvals[8];
-	mat[15]= dvals[2];
-	mat[16]= dvals[2];
-	mat[17]= dvals[17];
-	mat[18]= dvals[2];
-	mat[19]= dvals[13];
-	mat[20]= dvals[2];
-	mat[21]= dvals[18];
-	mat[22]= dvals[19];
-	mat[23]= dvals[20];
-	mat[24]= dvals[21];
-	mat[25]= dvals[25];
-	mat[26]= dvals[26];
-	mat[27]= dvals[27];
-	mat[28]= dvals[5];
-	mat[29]= dvals[9];
-	mat[30]= dvals[10];
-	mat[31]= dvals[31];
-	mat[32]= dvals[32];
-	mat[33]= dvals[33];
-	mat[34]= dvals[34];
-	mat[35]= dvals[8];
-	mat[36]= dvals[9];
-	mat[37]= dvals[6];
-	mat[38]= dvals[38];
-	mat[39]= dvals[39];
-	mat[40]= dvals[40];
-	mat[41]= dvals[41];
-	mat[42]= dvals[8];
-	mat[43]= dvals[7];
-	mat[44]= dvals[10];
-	mat[45]= dvals[45];
-	mat[46]= dvals[46];
-	mat[47]= dvals[47];
-	mat[48]= dvals[48];
-	
-	document.getElementById("outputbox").innerHTML += "Round 2 after P1's B is blocked\r\n";
-	InitA();
-	makeValuePositive();
-	Solve();
-	ValueOptimalStrategies();
-	display(form);
-	
-	r2gb2ev = foundev;
-	
-	dvals[0]=form.p1r2a.value;
-	dvals[1]=form.p1r2b.value;
-	dvals[2]=form.p1r2k.value;
-	dvals[3]=form.p1r2g.value;
-	dvals[5]=form.p16.value;
-	dvals[6]=form.p14.value;
-	dvals[7]=form.p12.value;
-	dvals[8]=-1*form.p2r2a.value;
-	dvals[9]=-1*form.p2r2b.value;
-	dvals[10]=-1*form.p2r2k.value;
-	dvals[11]=-1*form.p2r2g.value;
-	dvals[12]=-1*form.p26.value;
-	dvals[13]=-1*form.p24.value;
-	dvals[14]=-1*form.p22.value;
-	dvals[15]=form.p1gba.value;
-	dvals[16]=form.p1gbb.value;
-	dvals[17]=form.p1gbk.value;
-	dvals[18]=-1*form.p2gba.value;
-	dvals[19]=-1*form.p2gbb.value;
-	dvals[20]=-1*form.p2gbk.value;
-	dvals[21]=form.giworthp1.value;
-	dvals[22]=form.giworthp2.value;
-	
-	dvals[18]=0;
-	dvals[19]=0;
-	dvals[20]=0;
-	
-	if(gbriskp1){
-		dvals[18]=-1*form.p2gba.value;
-		dvals[19]=-1*form.p2gbb.value;
-		dvals[20]=-1*form.p2gbk.value;
-	}
-	if(gbriskp2){
-		dvals[15]=form.p1gba.value;
-		dvals[16]=form.p1gbb.value;
-		dvals[17]=form.p1gbk.value;
-	}
-	if(form.bypercents.value){
-		for(i=0;i<23;i++){
-			dvals[i] = dvals[i]/280.0;
-		}
-	}
-	
-	mat[0]= dvals[0];
-	mat[1]= dvals[9];
-	mat[2]= dvals[0];
-	mat[3]= dvals[15];
-	mat[4]= dvals[12];
-	mat[5]= dvals[0];
-	mat[6]= dvals[0];
-	mat[7]= dvals[1];
-	mat[8]= dvals[9];
-	mat[9]= dvals[1];
-	mat[10]= dvals[16];
-	mat[11]= dvals[1];
-	mat[12]= dvals[1];
-	mat[13]= dvals[14];
-	mat[14]= dvals[8];
-	mat[15]= dvals[2];
-	mat[16]= dvals[2];
-	mat[17]= dvals[17];
-	mat[18]= dvals[2];
-	mat[19]= dvals[13];
-	mat[20]= dvals[2];
-	mat[21]= dvals[18];
-	mat[22]= dvals[19];
-	mat[23]= dvals[20];
-	mat[24]= dvals[21];
-	mat[25]= dvals[25];
-	mat[26]= dvals[26];
-	mat[27]= dvals[27];
-	mat[28]= dvals[5];
-	mat[29]= dvals[9];
-	mat[30]= dvals[10];
-	mat[31]= dvals[31];
-	mat[32]= dvals[32];
-	mat[33]= dvals[33];
-	mat[34]= dvals[34];
-	mat[35]= dvals[8];
-	mat[36]= dvals[9];
-	mat[37]= dvals[6];
-	mat[38]= dvals[38];
-	mat[39]= dvals[39];
-	mat[40]= dvals[40];
-	mat[41]= dvals[41];
-	mat[42]= dvals[8];
-	mat[43]= dvals[7];
-	mat[44]= dvals[10];
-	mat[45]= dvals[45];
-	mat[46]= dvals[46];
-	mat[47]= dvals[47];
-	mat[48]= dvals[48];
-	
-	document.getElementById("outputbox").innerHTML += "Round 2 after P2's B is blocked\r\n";
-	InitA();
-	makeValuePositive();
-	Solve();
-	ValueOptimalStrategies();
-	display(form);
-	
-	r2gb1ev = foundev;
-	
-	dvals[0]=form.p1r1a.value;
-	dvals[1]=form.p1r1b.value;
-	dvals[2]=form.p1r1k.value;
-	dvals[3]=form.p1r1g.value;
-	dvals[8]=-1*form.p2r1a.value;
-	dvals[9]=-1*form.p2r1b.value;
-	dvals[10]=-1*form.p2r1k.value;
-	dvals[11]=-1*form.p2r1g.value;
-	dvals[12]=-1*form.p26.value;
-	dvals[13]=-1*form.p24.value;
-	dvals[14]=-1*form.p22.value;
-	dvals[15]=form.p1gba.value;
-	dvals[16]=form.p1gbb.value;
-	dvals[17]=form.p1gbk.value;
-	dvals[18]=-1*form.p2gba.value;
-	dvals[19]=-1*form.p2gbb.value;
-	dvals[20]=-1*form.p2gbk.value;
-	dvals[21]=form.giworthp1.value;
-	dvals[22]=form.giworthp2.value;
-	
-	/*
-	if(gbriskp1){
-		dvals[18]=-1*form.p2gba.value;
-		dvals[19]=-1*form.p2gbb.value;
-		dvals[20]=-1*form.p2gbk.value;
-	}
-	if(gbriskp2){
-		dvals[15]=form.p1gba.value;
-		dvals[16]=form.p1gbb.value;
-		dvals[17]=form.p1gbk.value;
-	}
-	*/
-	
-	mat[0]= r2ev;
-	mat[1]= dvals[9];
-	mat[2]= dvals[0];
-	mat[3]= dvals[15];
-	mat[4]= dvals[12];
-	mat[5]= dvals[0];
-	mat[6]= dvals[0];
-	mat[7]= dvals[1];
-	mat[8]= r2ev;
-	mat[9]= dvals[1];
-	mat[10]= r2gb1ev;
-	mat[11]= dvals[1];
-	mat[12]= dvals[1];
-	mat[13]= dvals[14];
-	mat[14]= dvals[8];
-	mat[15]= dvals[2];
-	mat[16]= r2ev;
-	mat[17]= dvals[17];
-	mat[18]= dvals[2];
-	mat[19]= dvals[13];
-	mat[20]= dvals[2];
-	mat[21]= dvals[18];
-	mat[22]= r2gb2ev;
-	mat[23]= dvals[20];
-	mat[24]= r2ev;
-	mat[25]= dvals[25];
-	mat[26]= dvals[26];
-	mat[27]= dvals[27];
-	mat[28]= dvals[5];
-	mat[29]= dvals[9];
-	mat[30]= dvals[10];
-	mat[31]= dvals[31];
-	mat[32]= dvals[32];
-	mat[33]= dvals[33];
-	mat[34]= dvals[34];
-	mat[35]= dvals[8];
-	mat[36]= dvals[9];
-	mat[37]= dvals[6];
-	mat[38]= dvals[38];
-	mat[39]= dvals[39];
-	mat[40]= dvals[40];
-	mat[41]= dvals[41];
-	mat[42]= dvals[8];
-	mat[43]= dvals[7];
-	mat[44]= dvals[10];
-	mat[45]= dvals[45];
-	mat[46]= dvals[46];
-	mat[47]= dvals[47];
-	mat[48]= dvals[48];
-	
-	document.getElementById("outputbox").innerHTML = "Round 1\r\n";
-	InitA();
-	makeValuePositive();
-	Solve();
-	ValueOptimalStrategies();
-	display(form);
-	
-}
+
 
 
 
